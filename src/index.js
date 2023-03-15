edba5077c53dd83be385a7a8f504762230b67470
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-03-15 12:33:57 +0530

    v5

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
-function App() {
-    return (
-     
-      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-     
-    )
-  }
-  export default App
+
 
 
 
