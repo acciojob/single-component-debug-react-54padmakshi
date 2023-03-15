74af9f29cd63a5f884f911ab1bb21088ec71f51f
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2023-03-15 12:13:35 +0530

    v3

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+
+
 ReactDOM.render(App, document.getElementById("root"))
 
